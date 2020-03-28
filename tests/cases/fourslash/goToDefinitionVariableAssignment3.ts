f6f8184472bb1e7b908c8f7ebd070d5a84192261
jeffy-g <hirotom1107@gmail.com>
2020-03-29 01:06:07 +0900

    moved fourslash test from "fourslash-server" to "fourslash" (native
    and improved regex (src/services/completions.ts

--- tests/cases/fourslash/goToDefinitionVariableAssignment3.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="fourslash.ts" />
-
-// @filename: foo.ts
-////const /*def1*/Foo = module./*def2*/exports = function () {}
-////Foo.prototype.bar = function() {}
-////new [|Foo/*ref*/|]();
-
-goTo.file("foo.ts");
-verify.goToDefinition("ref", ["def1", "def2"]);
