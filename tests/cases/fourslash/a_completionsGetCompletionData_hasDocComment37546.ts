cf0acc6bca11c06a837235e675b72bb4d603c81d
jeffy-g <hirotom1107@gmail.com>
2020-03-29 06:15:56 +0900

    moved fourslash test for PR #37546 to fourslash native category
    review of regex

---
+++ tests/cases/fourslash/a_completionsGetCompletionData_hasDocComment37546.ts
@@ -0,0 +1,83 @@
+/// <reference path="./fourslash.ts"/>
+
+//
+// fourslash test: src/services/completions.ts#getCompletionData (#37546)
+//
+// TIP: available marker characters - "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz$1234567890_"
+//  see src/harness/fourslashImpl.ts#parseFileContent
+
+// tests/cases/fourslash/server/openFileWithSyntaxKind.ts
+// tests/cases/fourslash/server/jsdocTypedefTagNamespace.ts
+// tests/cases/fourslash/server/tsxIncrementalServer.ts
+// tests/cases/fourslash/server/jsdocTypedefTag.ts
+
+// gulp runtests --runners=fourslash-server
+
+
+/////**
+//// +/*1*/
+//// */
+////function a0(s: string) {}
+////
+
+// gulp runtests --runners=fourslash-server
+
+/**
+ *
+ */
+const jsDocTagNames = [
+    "abstract",        "access",          "alias",           "argument",        "async",           "augments",        "author",          "borrows",         "callback",        "class",
+    "classdesc",       "constant",        "constructor",     "constructs",      "copyright",       "default",         "deprecated",      "description",     "emits",           "enum",
+    "event",           "example",         "exports",         "extends",         "external",        "field",           "file",            "fileoverview",    "fires",           "function",
+    "generator",       "global",          "hideconstructor", "host",            "ignore",          "implements",      "inheritdoc",      "inner",           "instance",        "interface",
+    "kind",            "lends",           "license",         "listens",         "member",          "memberof",        "method",          "mixes",           "module",          "name",
+    "namespace",       "override",        "package",         "param",           "private",         "property",        "protected",       "public",          "readonly",        "requires",
+    "returns",         "see",             "since",           "static",          "summary",         "template",        "this",            "throws",          "todo",            "tutorial",
+    "type",            "typedef",         "var",             "variation",       "version",         "virtual",         "yields"
+];
+const jsDocTags = jsDocTagNames.map(name => "@" + name);
+
+
+// line 2: [ +|c|]
+verify.completions({
+    marker: "1",
+    exact: undefined // or []
+});
+
+// line 2: [ +|c|] ->  [ +@|c|]
+// before the fix, jsdoc tag names was listed but no longer appears
+edit.insert("@");
+verify.completions({
+    marker: "1",
+    exact: undefined
+});
+
+// line 2: [ +@|c|] -> [ *|c|]
+// before the fix, jsdoc tags was listed but no longer appears
+edit.replaceLine(1, " *");
+// goTo.position({ line: 1, character: 2 });
+verify.completions({
+    // marker: "1", // marker is not available
+    exact: undefined
+});
+
+// line 2: [ *|c|] -> [ *@|c|]
+// this behavior does not by "hasDocComment" section
+edit.insert("@");
+verify.completions({
+    includes: jsDocTagNames
+});
+
+// line 2: [ *@|c|] -> [ * |c|]
+// jsdoc tags are listed when there is more than one whitespace after "*"
+edit.replaceLine(1, " * ");
+verify.completions({
+    includes: jsDocTags
+});
+
+// line 2: [ * |c|] -> [ * @|c|]
+// jsdoc tag names will be listed
+edit.insert("@");
+verify.completions({
+    includes: jsDocTagNames
+});
--- tmp/regex-summary.ts
+++ tmp/regex-summary.ts
@@ -12,17 +12,17 @@
 
 // https://regex101.com/r/Qe4Mqs/3
 /**
- * ```perl
- *^(?:                # non-capturing group $1
- *  \s*               # has whitespace ahead?
- *  (?:               # non-capturing group $2
- *    [*\s]+(?=\s+)   # A string that is a mixture of "*" and whitespace terminated by one or more whitespaces
- *  )?\s+             # adjust for previous anchor
- *  |                 # or
- *  \s*\/\*\*\s+      # e.g - [ /** ]
- *)                   # end non-capturing group $1
+ * ```php
+ *^(?:              # non-capturing group $1
+ *  \s*             # has whitespace ahead?
+ *  (?:             # non-capturing group $2
+ *    [*\s]+(?=\s)  # A string that is a mixture of "*" and whitespace terminated by one or more whitespaces
+ *  )?\s+           # adjust for previous anchor (end non-capturing group $2)
+ *  |               # or
+ *  \s*\/\*\*\s+    # e.g - [ /** ]
+ *)                 # end non-capturing group $1
  *(
- *  @                 # has triggerCharacter? (capturing group $1)
+ *  @               # has triggerCharacter? (capturing group $1)
  *)?$
  * ```
  */
