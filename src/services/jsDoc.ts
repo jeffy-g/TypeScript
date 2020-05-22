a27fe765f611dd1c928073300b8c1f66fc61e145
jeffy-g <hirotom1107@gmail.com>
2020-05-23 00:56:06 +0900

    getDocCommentTemplateAtPosition - format jsdoc

--- src/services/jsDoc.ts
+++ src/services/jsDoc.ts
@@ -230,25 +230,28 @@ namespace ts.JsDoc {
     /**
      * Checks if position points to a valid position to add JSDoc comments, and if so,
      * returns the appropriate template. Otherwise returns an empty string.
+     * 
      * Valid positions are
-     *      - outside of comments, statements, and expressions, and
-     *      - preceding a:
-     *          - function/constructor/method declaration
-     *          - class declarations
-     *          - variable statements
-     *          - namespace declarations
-     *          - interface declarations
-     *          - method signatures
-     *          - type alias declarations
+     * 
+     *   - outside of comments, statements, and expressions, and
+     *   - preceding a:
+     *     - function/constructor/method declaration
+     *     - class declarations
+     *     - variable statements
+     *     - namespace declarations
+     *     - interface declarations
+     *     - method signatures
+     *     - type alias declarations
      *
      * Hosts should ideally check that:
-     * - The line is all whitespace up to 'position' before performing the insertion.
-     * - If the keystroke sequence "/\*\*" induced the call, we also check that the next
-     * non-whitespace character is '*', which (approximately) indicates whether we added
-     * the second '*' to complete an existing (JSDoc) comment.
+     *   - The line is all whitespace up to 'position' before performing the insertion.
+     *   - If the keystroke sequence "/&#42;&#42;" induced the call, we also check that the next
+     * non-whitespace character is '&#42;', which (approximately) indicates whether we added
+     * the second '&#42;' to complete an existing (JSDoc) comment.
+     * 
      * @param fileName The file in which to perform the check.
      * @param position The (character-indexed) position in the file where the check should
-     * be performed.
+     *     be performed.
      */
     export function getDocCommentTemplateAtPosition(newLine: string, sourceFile: SourceFile, position: number): TextInsertion | undefined {
         const tokenAtPos = getTokenAtPosition(sourceFile, position);
